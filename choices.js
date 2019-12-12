6bc3ff56ac3dcc50deb14f1a277775db2a89fa3d
Damian Ruiz <nrt.damian@gmail.com>
2019-12-12 00:27:40 -0500

    Arrays to feed inquirer

---
+++ choices.js
@@ -0,0 +1,31 @@
+const main = [
+    "View records",
+    "Add records",
+    "Update records",
+    "Quit"   
+];
+
+const view = [
+    "View all employees",
+    "View all roles",
+    "View all departments",
+    "View employees by manager",
+    "View total utilized budget"
+]
+
+const add = [
+    "Add employee",
+    "Add department",
+    "Add role"
+]
+
+const update = [
+    "Update employee role",
+]
+
+module.exports = {
+    main: main,
+    view: view,
+    add: add,
+    update: update,
+}
